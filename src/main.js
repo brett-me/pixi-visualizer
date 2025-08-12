610da76d458405cea562e955904c78a12190e336
brett-me <its-me-brett@protonmail.com>
2025-08-12 15:22:27 +0200

    util: scaleToWindow

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import * as PIXI from "pixi.js";
 import { setupApp } from "./setupApp";
 import { loadAssets } from "./loadAssets";
+import { scaleToWindow } from "./utils/scaleToWindow";
+
 
 (async () => {
     const app = await setupApp();
     const { bg1, bg2, bg3 } = await loadAssets();
+    scaleToWindow(app, bg1);
     app.stage.addChild(bg1);
+
 })();
---
+++ src/utils/scaleToWindow.js
@@ -0,0 +1,23 @@
+export function scaleToWindow(app, ...objs) {
+    if (objs.length === 1 && Array.isArray(objs[0])) {
+        objs = objs[0];
+    }
+
+    function resize() {
+        objs.forEach(obj => {
+            obj.width = app.renderer.width;
+            obj.height = app.renderer.height;
+        });
+    }
+
+    resize();
+
+    let resizeTimeout;
+    window.addEventListener("resize", () => {
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(() => {
+            app.renderer.resize(window.innerWidth, window.innerHeight);
+            resize();
+        }, 100);
+    });
+}
