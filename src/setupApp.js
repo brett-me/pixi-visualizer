3bf259d8b34bb9d090bb93d6e5724bf9b0801207
brett-me <its-me-brett@protonmail.com>
2025-08-12 12:47:27 +0200

    add setupApp function (inits pixi app)

---
+++ src/setupApp.js
@@ -0,0 +1,19 @@
+import * as PIXI from "pixi.js";
+
+export async function setupApp() {
+    const app = new PIXI.Application();
+    await app.init({
+        resizeTo: window,
+        backgroundColor: "#000",
+        hello: true,
+    });
+    document.body.appendChild(app.canvas);
+    
+    if (location.hostname === "localhost") {
+        const { initDevtools } = await import("@pixi/devtools");
+        initDevtools({ app });
+        window.__PIXI_APP__ = app;
+    };
+
+    return app;
+}
